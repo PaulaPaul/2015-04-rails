5f4dadbe4b235f9ade8b58e91dccf22299c7e510
PaulaTTS <paula@techtalentsouth.com>
2015-05-20 14:24:01 +0000

    prep for PT class

--- RailsATM/config/application.rb
+++ RailsATM/config/application.rb
@@ -20,7 +20,7 @@ module RailsATM
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    # config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
   end
   
   # Create a place to store information for the duration of the session
